d8183e141f6057224f0de313191fad4bbcb60f7a
AdRyan49 <adamgryan06@icloud.com>
2025-10-22 20:23:32 +0100

    Update CPU.java

--- CPU.java
+++ CPU.java
@@ -1,1 +1,10 @@
+public class CPU {
+    static {
+        // Loading Mark's Library
+        System.loadLibrary("libsysinfo"); //libsysinfo.so
+    }
 
+    public void printString(String x) {
+        System.out.println("test: "+x);
+    }
+}
