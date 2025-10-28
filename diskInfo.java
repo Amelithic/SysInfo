ce335eb3779df84cdc2df8932155892beb8e2631
1810anav <1810anav@gmail.com>
2025-10-28 10:43:56 +0000

    Made table

--- diskInfo.java
+++ diskInfo.java
@@ -16,13 +16,14 @@ public class diskInfo {
     public native long getUsed (int disk);
     public native long getAvailable (int disk);
 
-    public void make_bar(double percentage, int size){
+    public String make_bar(double percentage, int size){
         int progress = (int)Math.floor(percentage*size);
+        String temp = "";
         for(int i = 0; i<progress; i++)
-            System.out.print("\u2593");
+            temp += "\u2593";
         for(int j = 0; j<size-progress; j++)
-            System.out.print("\u2591");
-        System.out.println();
+            temp += "\u2591";
+        return temp;
     }
 
     public static String print_unit(long kilobytes){
@@ -43,21 +44,35 @@ public class diskInfo {
     public void print_table() {
         read();
         System.out.println(Style.BOLD + Style.CYAN + "Number of disks: " + diskCount() + "\n" + Style.RESET );
-        System.out.println(Style.BLUE + "_____________________________________________________" + Style.RESET);
-        System.out.printf(Style.BLUE + "| %-15s | %-8s | %-20s |\n" + Style.RESET, "Name", "Size", "Bar");
+        System.out.println(Style.BLUE + "------------------------------------------------------------------------------------------------------" + Style.RESET);
+        System.out.printf(Style.BLUE + "| %-15s | %-12s | %-12s | %-50s |\n" + Style.RESET, "Name", "Size", "Used", "Bar");
+        System.out.println(Style.BLUE + "------------------------------------------------------------------------------------------------------" + Style.RESET);
         for (int i=0; i < diskCount(); i++) {
             long total = getTotal(i);
             long used = getUsed(i);
 
-            System.out.printf("", getName(i));
-            System.out.print("Size: ");
-            print_unit(total);
-            System.out.println();
-            System.out.printf("Used: ");
-            print_unit(used);
-            System.out.println();
+            System.out.printf(Style.BLUE + "|" + Style.YELLOW + " %-15s " + Style.BLUE + "|" + Style.YELLOW + " %-12s " + Style.BLUE + "|" + Style.YELLOW + " %-12s " + Style.BLUE + "|" + Style.RESET, getName(i), print_unit(total), print_unit(used));
             double percentage = (double)used/total;
-            make_bar(percentage, 50);
+
+            if(percentage < 0.2){
+                System.out.print(Style.RESET + " " + make_bar(percentage, 50) + Style.BLUE + " |");
+            }
+
+            else if(percentage >= 0.2 && percentage <0.6){
+                System.out.print(Style.GREEN + " " + make_bar(percentage, 50) + Style.GREEN + " |");
+            }
+
+            else if(percentage >= 0.6 && percentage <0.8){
+                System.out.print(Style.CYAN + " " + make_bar(percentage, 50) + Style.BLUE + " |");
+            }
+
+            else{
+                System.out.print(Style.RED + " " + make_bar(percentage, 50) + Style.BLUE + " |");
+            }
+
+            System.out.println();
         }
+        System.out.println(Style.BLUE + "------------------------------------------------------------------------------------------------------" + Style.RESET);
+
     }
 }
