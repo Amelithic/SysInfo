c405cceaadca4bd6bafa77086c46e2f0068cdca6
Amelithic <amelie.mccarthy058@gmail.com>
2025-10-29 22:03:46 +0000

    Minor styling changes - cohesive project style

--- SysInfo.java
+++ SysInfo.java
@@ -15,7 +15,7 @@ public class SysInfo {
             switch (selection) {
                 case 1:
                     Style.resetScreen();
-                    System.out.println("CPU");
+                    System.out.println(Style.RED+Style.BOLD+"CPU"+Style.RESET);
                     System.out.println();//newline
 
                     cpuInfo testCPU = new cpuInfo();
@@ -68,7 +68,7 @@ public class SysInfo {
 
                 case 3:
                     Style.resetScreen();
-                    System.out.println("Disk");
+                    System.out.println(Style.RED+Style.BOLD+"Disk"+Style.RESET);
                     System.out.println();//newline
 
                     diskInfo testDisk = new diskInfo();
@@ -81,7 +81,7 @@ public class SysInfo {
 
                 case 4:
                     Style.resetScreen();
-                    System.out.println("Attached Devices");
+                    System.out.println(Style.RED+Style.BOLD+"Attached Devices"+Style.RESET);
                     System.out.println();//newline
 
                     pciInfo testPCI = new pciInfo();
